e6ffb2144dccbfd9388fb5edee5d714fc18891a3
NinaLJ <787325997@qq.com>
2016-11-21 16:22:24 +0800

    新更新lijiang

---
+++ app/src/main/java/com/daomaidaomai/islandtrading/MainActivity.java
@@ -0,0 +1,14 @@
+package com.daomaidaomai.islandtrading;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
