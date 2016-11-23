e7d5762a0d923a3a20a217be1258c61522efee2d
xujihui <805991134@qq.com>
2016-11-23 10:51:00 +0800

    页面代码已完成1.0

--- IslandTrading/app/src/main/java/com/daomaidaomai/islandtrading/MainActivity.java
+++ IslandTrading/app/src/main/java/com/daomaidaomai/islandtrading/MainActivity.java
@@ -8,10 +8,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.set);
-=======
-        setContentView(R.layout.my_publish_layout);
->>>>>>> c41b39b7fb7700bb12c4d7ec408558294d599c2f
+
+        setContentView(R.layout.setting_personal_information2);
+
     }
 }
