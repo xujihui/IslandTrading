b5a8f073702cb7ff01ee175f3fdedcc482201ca7
LuHoney7 <441007713@qq.com>
2016-12-01 09:57:42 +0800

    聊天1

---
+++ 实时聊天技术攻关/nim_demo/demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 29;
+  public static final String VERSION_NAME = "3.1.0";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2016-11-28 16:21:27";
+  public static final String GIT_REVISION = "";
+}
