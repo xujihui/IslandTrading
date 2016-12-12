e713b4806c4c742a237857006713c9823229ec6e
LuHoney7 <441007713@qq.com>
2016-12-12 12:08:42 +0800

    ChatActivity

---
+++ 实时聊天技术攻关/EaseUILevelThree/app/src/main/java/com/example/lenovo/easeuilevelthree/ChatActivity.java
@@ -0,0 +1,44 @@
+package com.example.lenovo.easeuilevelthree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.hyphenate.easeui.ui.EaseChatFragment;
+
+public class ChatActivity extends AppCompatActivity {
+    // 当前聊天的 ID
+    private String mChatId;
+    private EaseChatFragment chatFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_chat);
+
+        // 这里直接使用EaseUI封装好的聊天界面
+        chatFragment = new EaseChatFragment();
+        // 将参数传递给聊天界面
+        chatFragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().add(R.id.ec_layout_container, chatFragment).commit();
+
+        initView();
+    }
+    /**
+     * 初始化界面
+     */
+    private void initView() {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
