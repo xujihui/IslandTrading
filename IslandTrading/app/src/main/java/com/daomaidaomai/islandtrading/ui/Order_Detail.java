71bb83efdb2960c9956e08c16fddaa6fc08889a1
liuxin2017 <1056669678@qq.com>
2016-12-20 08:56:23 +0800

    优化

--- IslandTrading/app/src/main/java/com/daomaidaomai/islandtrading/ui/Order_Detail.java
+++ IslandTrading/app/src/main/java/com/daomaidaomai/islandtrading/ui/Order_Detail.java
@@ -1,23 +1,33 @@
 package com.daomaidaomai.islandtrading.ui;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.daomaidaomai.islandtrading.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Order_Detail extends Activity {
 
+    //链接XML
     private LinearLayout Back;
+    private TextView order_tv_name;
+    private TextView order_tv_price;
 
+    //点击事件监听
     public View.OnClickListener mylistener = new View.OnClickListener() {
         public void onClick(View v) {
             switch (v.getId()) {
@@ -31,9 +41,12 @@ public class Order_Detail extends Activity {
         }
     };
 
+    //回调函数
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //标题栏透明化
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
@@ -47,11 +60,21 @@ public class Order_Detail extends Activity {
             window.setNavigationBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_orderdetail);
+        findview();
+        setMylistener();
+    }
 
+    //绑定XML
+    private void findview(){
         Back = (LinearLayout) findViewById(R.id.back);
+        order_tv_name = (TextView) findViewById(R.id.order_tv_name);
+        order_tv_price = (TextView)findViewById(R.id.order_tv_price);
 
+    }
+
+    //监听函数
+    private void setMylistener(){
         Back.setOnClickListener(mylistener);
-
 
     }
 }
